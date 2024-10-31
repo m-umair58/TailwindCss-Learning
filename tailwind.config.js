e3dbb53a2b904ad555141dcdca54ca47a4c7acd1
Muhammad Umair <mianumair1958@gmail.com>
2024-10-31 19:32:53 +0500

    tailwind learning repo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:"#ESESD5",
+          200:"#888883"
+        }
+
+      }
+    },
+  },
+  plugins: [],
+}
+
